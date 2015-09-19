c5c02451f5cbf92cd90a78ceed49ad66b22fe6d2
Rajul Arora <r9arora@uwaterloo.ca>
2015-09-19 13:52:10 -0400

    Contact manager update

--- Contacts/Contacts/ContactManager.swift
+++ Contacts/Contacts/ContactManager.swift
@@ -1,9 +1,11 @@
 //
-//  ContantManager.swift
+//  ContactManager.swift
 //  Contacts
 //
 //  Created by Rajul Arora on 2015-09-19.
 //  Copyright © 2015 Rajul Arora. All rights reserved.
 //
 
-import Foundation
+class ContactManager: NSObject {
+
+}
